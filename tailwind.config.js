060cc5536d2ae5aa707e0bba558616c80d85def4
afrozalima <128209713+afrozalima@users.noreply.github.com>
2023-05-25 12:34:09 +0600

    Add files via upload
    Image Vult

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+    colors:{
+      "top-nav":"#232228",
+      "sub-nav":"#35353d",
+      "f-color":"#FFFFFF",
+      "main-body":"#3c3c44",
+      "search":"#808080"
+    }
+  },
+  plugins: [],
+}
+
